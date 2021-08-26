973bdbc4631c6341e3439a6b50888c5bc51d8161
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2021-08-26 17:45:18 +0700

    Up code

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -2,3 +2,8 @@ import Sample from "./modules/_sample";
 $(document).ready(() => {
   let sample = new Sample();
 });
+
+import Banner from "./modules/_banner";
+$(document).ready(() => {
+  let banner = new Banner();
+})
